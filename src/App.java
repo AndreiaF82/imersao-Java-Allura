38412734d2c4fd896f973916a635d68cfab9a38d
AndreiaF82 <andreiaf.2005@gmail.com>
2023-03-30 17:53:31 -0300

    aula2Java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandler;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
@@ -31,22 +32,26 @@ public class App {
 
 
         // exibir e manipular dados 
-
+   
+        var geradora = new GeradoraDeFigurinhas();
     for (Map<String, String> filme : listaDeFilmes) {
-        
-        System.out.println(filme.get("title"));
-        System.out.println(filme.get("image"));
-        System.out.println(filme.get("imDbRating"));
-        System.out.println();
+
+        var urlImagem = filme.get("image");
+        String titulo = filme.get("title");
+
+        InputStream inputStream = new URL(urlImagem).openStream();
+        String nomeArquivo = titulo + " .png";
+
+         
+       geradora.cria(inputStream, nomeArquivo);
+
+        System.out.println(titulo);
+              System.out.println();
         
     }
             
         }
-
-
-
-
         
-
+        
     }
 
